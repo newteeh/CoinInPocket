544447c0fd761ff9bb43f18504be409cf6509711
newteeh <danya.pavlov.54@mail.ru>
2022-10-27 12:54:03 +0500

    second

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 const select1 = new ItcCustomSelect('#select-1');
+
+// $(document).ready(function(){
+//     $('.header__burger').click(function(event){
+//         $('.header__burger, .header__menu').toggleClass('active');
+//         $('body').toggleClass('lock');
+//     });
+// });
+// console.log('HelloWorld');
+let menuBtn = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+menuBtn.addEventListener('click',function(){
+    console.log('WTF?')
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+})
