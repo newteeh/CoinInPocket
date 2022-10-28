f3f00ea1f2a093cdbf7e43a876fa02bb049722f4
newteeh <danya.pavlov.54@mail.ru>
2022-10-28 12:54:12 +0500

    third

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ menuBtn.addEventListener('click',function(){
     menu.classList.toggle('active');
     menuBtn.classList.toggle('active');
 })
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Отображение значения ползунка по умолчанию
+
+// Обновите текущее значение ползунка (при каждом перетаскивании маркера ползунка)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+var slider2 = document.getElementById("myRange2");
+var output2 = document.getElementById("demo2");
+output2.innerHTML = slider2.value; // Отображение значения ползунка по умолчанию
+
+// Обновите текущее значение ползунка (при каждом перетаскивании маркера ползунка)
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
