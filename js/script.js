3188925e4a1fd078b52dcceff694be878dc65d46
newteeh <danya.pavlov.54@mail.ru>
2022-10-31 12:53:53 +0500

    fifth

--- js/script.js
+++ js/script.js
@@ -23,3 +23,12 @@ output.innerHTML = slider.value; // Display the default slider value
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
+var slider1 = document.getElementById("myRange1");
+var output1 = document.getElementById("demo1");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider1.oninput = function() {
+    output1.innerHTML = this.value;
+}
